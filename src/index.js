22e9ad2d1ba624b5a47c86749b67bec04a415950
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2023-04-19 16:19:52 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -4,71 +4,64 @@ import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css" ;
 
-
-console.log('hello')
-
 const refs = {
     form: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
-    guard: document.querySelector('.js-guard'),
+    // guard: document.querySelector('.js-guard'),
     loadMore: document.querySelector('.load-more')
 }
 
 const API_KEY = '35439381-dc6c31f5e4218074de9a0ab23';
 const BASE_URL = 'https://pixabay.com/api/';
 
-
-
 let searchQuery = '';
 let page = 1;
 let totalHits = 0;
-
+let lightbox = new SimpleLightbox('.gallery a');
 
 refs.form.addEventListener('submit', onSearch)
 refs.loadMore.addEventListener('click', onLoadMore)
 
-
-async function onLoadMore(event) {
+async function onLoadMore() {
   page += 1;
 
   const data = await axiosGet(searchQuery);
   totalHits += data.hits.length
-  console.log(totalHits)
+  
   if (totalHits>=500 || data.total===totalHits) {
-    refs.loadMore.classList.toggle("is-hidden");
+    refs.loadMore.classList.add("is-hidden");
     Notify.warning("We're sorry, but you've reached the end of search results.");
     }
   const markUp = await createMarkup(data.hits);
     
   appendMarkup(createMarkup(data.hits))
- 
+  lightbox.refresh();
 }
 
 async function onSearch(event) {
-  event.preventDefault()
+    event.preventDefault()
+    clearContainer();  
 
-  clearContainer();  
-
-  searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-  page = 1;
+    searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+    page = 1;
   
-   if (!searchQuery) {
+    if (!searchQuery) {
         Notify.info("Sorry. Please enter a search.")
         return
-   }
-  
-  const data = await axiosGet(searchQuery);
-  
-  const markUp = await createMarkup(data.hits);
+    }
+    const data = await axiosGet(searchQuery);
+    const markUp = await createMarkup(data.hits);
 
-  appendMarkup(markUp)
-  if (data.total < 40) {
-    refs.loadMore.classList.toggle("is-hidden");
-    Notify.warning("We're sorry, but you've reached the end of search results.");
-  }
+    appendMarkup(markUp)
+    lightbox.refresh();
+    Notify.info(`Hooray! We found ${data.totalHits} images.`)
+    if (data.total < 40) {
+      Notify.warning("We're sorry, but you've reached the end of search results.");
+      refs.loadMore.classList.add("is-hidden");
+      return
+    }
     totalHits += data.hits.length;
-    console.log(totalHits)
-      refs.loadMore.classList.toggle("is-hidden");
+    refs.loadMore.classList.remove("is-hidden");
 }
 
 async function axiosGet(search) {
@@ -79,10 +72,10 @@ async function axiosGet(search) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: 140
+        per_page: 40
       },
     })
-    console.log(responce)
+    
  if (!responce.data.total) {
             throw new Error(Notify.failure('Sorry, there are no images matching your search query. Please try again.'))
  }
@@ -118,13 +111,8 @@ function createMarkup(arr) {
 
 function appendMarkup(markup) {
   refs.gallery.insertAdjacentHTML('beforeend', markup);
-  let lightbox = new SimpleLightbox('.gallery a');
 }
 
 function clearContainer() {
   refs.gallery.innerHTML = '';
 }
-
-
-
-console.log('ok')
